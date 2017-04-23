0016dc408b0870611ddaf2beb9a009c2f408ca9f
Ajinkya Farsole <farsoleajinkya@gmail.com>
2017-04-23 23:56:10 +0530

    Updated Code with Some String Operations

--- SamplePythonCodes/SamplePythonCodes/SimpleStringOperations.py
+++ SamplePythonCodes/SamplePythonCodes/SimpleStringOperations.py
@@ -1,2 +1,27 @@
 # The program to demostarte basic String Operations in Python
 
+import sys;
+
+FirstName=raw_input("First Name : \n");
+LastName=raw_input("Last Name : \n");
+
+# String Concate
+
+FullName = FirstName + " " + LastName;
+print("\nFull Name : " + FullName); 
+
+# String UPPERCASE
+
+UP_FirstName=FirstName.upper();
+print("\nCapital First Name : " + UP_FirstName);
+UP_LastName=LastName.upper();
+print("\nCapital Last Name : " + UP_LastName);
+
+# String LOWERCASE
+
+LOW_FirstName=FirstName.lower();
+print("\nLower First Name : " + LOW_FirstName);
+LOW_LastName=LastName.lower();
+print("\nLower Last Name : " + LOW_LastName);
+
+print("\n");
